d5ae617af73aee13e9de3726ea1beee483873c02
Rohan Pathak <90509336+rohanpathak333@users.noreply.github.com>
2024-08-13 14:23:27 +0000

    Comparison Operators

---
+++ 01_basics/04_Comparison.js
@@ -0,0 +1,17 @@
+console.log("2">1);
+console.log("02"> 1);
+
+console.log(null > 0); //false
+console.log(null == 0); //false
+console.log(null >= 0);// true  null is converted to 0
+//The reason is that an equality check == and comparisons > < >= <= work differently
+//Comparisons convert null to a number, treating it as 0.
+//That's why null >= 0 is true and null > 0 is false
+
+console.log(undefined == 0);//false
+console.log(undefined > 0);//false
+console.log(undefined < 0); //false
+
+// ===
+
+console.log("2" === 2); //false
