27fba234d45f4e8569db9db81a92dabdeafe18f9
Rohan Pathak <90509336+rohanpathak333@users.noreply.github.com>
2024-08-30 06:40:15 +0000

    For loop ,break and continue

---
+++ 05_iterations/one.js
@@ -0,0 +1,123 @@
+// for
+
+// for (let i = 0; i <=10; i++){
+//     const element = i;
+//     if(element == 5){
+//         console.log("5 is the best number")
+//     }
+//     console.log(element);
+
+// }
+
+// for(let i=0; i<=10; i++){
+//     console.log(`Outer loop value: ${i}`);
+//     for(let j=0;j<=10;j++){
+//         console.log(`Inner loop value ${j} and outer loop ${i}`);
+//     }
+    
+
+// }
+
+
+
+
+
+// for(let i=1; i<=10; i++){
+//         console.log(`Outer loop value: ${i}`);
+//         for(let j=1;j<=10;j++){
+//             // console.log(`Inner loop value ${j} and outer loop ${i}`);
+//             console.log(i + '*' + j + '=' + i*j);
+            
+    //     }
+        
+    // }
+
+
+// let myArray = ["flash", "batman", "superman"]
+// console.log(myArray.length);
+
+// for (let index = 0; index < myArray.length; index++) {
+//     const element = myArray[index];
+//     console.log(element);
+    
+    
+// }
+
+
+// break and continue
+
+// for(let index = 1; index <= 20; index++){
+//     if( index == 5){
+//         console.log(`Detected 5`);
+//         break
+        
+//     }
+//     console.log(`value of i is ${index}`);
+// }
+
+//output
+// value of i is 1
+// value of i is 2
+// value of i is 3
+// value of i is 4
+// Detected 5
+
+
+for(let index = 1; index <= 20; index++){
+        if( index == 5){
+            console.log(`Detected 5`);
+            continue
+            
+        }
+        console.log(`value of i is ${index}`);
+    }
+
+//     value of i is 1
+// value of i is 2
+// value of i is 3
+// value of i is 4
+// Detected 5
+// value of i is 6
+// value of i is 7
+// value of i is 8
+// value of i is 9
+// value of i is 10
+// value of i is 11
+// value of i is 12
+// value of i is 13
+// value of i is 14
+// value of i is 15
+// value of i is 16
+// value of i is 17
+// value of i is 18
+// value of i is 19
+// value of i is 20
+    
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
